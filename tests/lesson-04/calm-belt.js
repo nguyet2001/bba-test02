ec94b44f0faa926b2ad33367593ed81d4cf080c8
Test02-Anh Nguyet <luyenanhnguyet.ptit@gmail.com>
2026-09-17 01:55:36 +0700

    feat: add solution for calm belt challenge

---
+++ tests/lesson-04/calm-belt.js
@@ -0,0 +1,13 @@
+let navigatorName = "Luffy";
+let currentHakilevel =3 ;
+
+const Island_A = 15;
+const Island_B = 25;
+const Island_C = 40;
+
+let a= (Island_A + Island_B + Island_C);
+
+console.log (`Tổng lượng thịt tiêu thụ ở 3 đảo : ${a}`);
+console.log (`Trung bình tổng: ${a/3}`);
+console.log ("Thịt dư:", a%3);
+
