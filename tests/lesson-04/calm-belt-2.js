5809bc69264d9543f411be6ea32f8de1f2215d15
Test02-Anh Nguyet <luyenanhnguyet.ptit@gmail.com>
2026-09-17 02:04:43 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/calm-belt-2.js
@@ -0,0 +1,15 @@
+let devilFruit ="gomu-gomu";
+
+if (devilFruit === "gomu-gomu"){
+    console.log ("Gear 5 Unlocked!");
+}
+else if (devilFruit=== "mera-mera"){
+    console.log ("Flame Emperor!");
+}
+else if (devilFruit=== "ope-ope"){
+    console.log ("Room & Ope!");
+}
+else if (devilFruit=== "none"){
+    console.log ("Normal Human");
+}
+else console.log ("Unknown Devil Fruit");
